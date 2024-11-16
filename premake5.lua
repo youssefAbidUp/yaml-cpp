0bd140bf352b173db3d60c66529170c76181f5ad
Mohamed Amine Ouerghi <168843047+AmineOuerghi07@users.noreply.github.com>
2024-11-16 14:55:49 +0100

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "yaml-cpp"
+    kind "staticlib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+        
+        "include/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
